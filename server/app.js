a1c7b887d081ff2ab282c0e5e34d551665828c39
natedunn2230 <ndunn2230@gmail.com>
2022-02-05 12:13:39 -0500

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+if (process.env.NODE_ENV === "dev") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+}
+
+app.get("/", (req, res) => {
+  res.send("welcome to wordle");
+});
+
+app.get("/test", (req, res) => {
+  res.send("stuff");
+});
+
+app.listen(5000, () => {
+  console.log("app listening on port: ", 5000);
+});
