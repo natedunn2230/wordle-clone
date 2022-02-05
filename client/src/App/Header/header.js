22af404a2d6c813838a1f1ca245bd38b1bbb0ffb
XarosElite <polingj999@gmail.com>
2022-02-05 14:08:23 -0500

    Basic Restructuring

---
+++ client/src/App/Header/header.js
@@ -0,0 +1,14 @@
+import "./header.css";
+
+
+function header() {
+  return (
+    <div className="header">
+      
+    </div>
+  );
+}
+
+export default header;
+
+
---
+++ client/src/App/Keyboard/keyboard.js
@@ -0,0 +1,12 @@
+import "./keyboard.css";
+
+
+function keyboard() {
+  return (
+    <div className="keyboard">
+      
+    </div>
+  );
+}
+
+export default keyboard;
